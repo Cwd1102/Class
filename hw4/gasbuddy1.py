8ab4544901e89b54afd95120e6fa82f52ba9617a
Ouwen <ouwen0308@gmail.com>
2022-10-03 18:57:12 -0400

    Complete

--- hw4/gasbuddy1.py
+++ hw4/gasbuddy1.py
@@ -8,9 +8,6 @@ Description: finds the sum of squares of int input by user
 
 """
 
-from glob import iglob
-
-
 if __name__ == '__main__':
     
     #stores number of gas stations the user wants to input
@@ -19,25 +16,53 @@ if __name__ == '__main__':
     #stores gas prices for each gas station
     gas_list = list()
 
-    #user inputs prices for each gas station
-    for i in range(num_gas):
-        print("What is the gas price at station",i + 1)
-        gas_price = float(input(""))
-        gas_list.append(gas_price)
-    
+    station_list = list()
+
     low_gas = 0
 
     gas_station = 1
 
+    condition = True
 
-      #checks the lowest price
-    for i in range(len(gas_list)):
-        
-       if(gas_list[i] > low_gas):
-           low_gas = gas_list[i] 
+    name = True
+
+    biggest = 0
+
+    station = 0
+
+    var = 0
+
+    #user inputs prices for each gas station
+    if(num_gas < 0):
+        print("Invalid number of gas stations")
+    else:
+        while(name):
+            print("What is the gas price at station", station + 1)
+            gas_price = float(input(""))
+            if(gas_price < 0):
+                print("I wish, but please enter a valid price of gas.")
+            else:
+                gas_list.append(gas_price)
+                station_list.append(gas_price)
+                station += 1
+            
+            if(station == num_gas):
+                name = False
+
+        for i in range(len(gas_list)):
+            if(gas_list[i] > biggest):
+                biggest = gas_list[i]
+
+        for x in range(len(gas_list)):
+            for i in range(len(gas_list) - 1):
+                if(gas_list[i] > gas_list[i + 1]):
+                    gas_list[i + 1] , gas_list[i] = gas_list[i] , gas_list[i +1]
+                    low_gas = gas_list[0]
+
+        while(condition):
+            if(gas_list[0] == station_list[var]):
+                gas_station = var + 1
+                condition = False
+            var += 1
     
-        if:
-            gas_station += i
-            print(gas_station)
-    
-    print("The lowest price of gas found was station #", gas_station , "at $", low_gas)
+        print("The lowest price of gas found was station #%d at $%.2f" %(gas_station , low_gas) )
