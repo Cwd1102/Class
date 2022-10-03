680d92f37e3782e31371fa0b304594ab7aec2da4
Ouwen Dai <ouwen@gmail.com>
2022-10-03 13:55:49 -0400

    new

---
+++ hw4/sos.py
@@ -0,0 +1,30 @@
+"""
+File:    sos.py
+Author:  Ouwen Dai
+Date:    10/1/22
+Section: 56
+E-mail:  odai1@umbc.edu
+Description: finds the sum of squares of int input by user
+
+"""
+
+if __name__ == '__main__':
+    
+    #stores value for the number the user wishes to find the sos
+    sos = int(input("What number should we determine the S.O.S.?"))
+
+    #determins if user wants to exit program
+    if(sos == -1):
+        print("Thank you. Program over!")
+    
+    #breaks out of code if user inputs number > -1 
+    elif(sos < -1):
+        print("Invalid input. PLease enter a value > 1")
+    
+    #calculates the sos of number
+    else:
+        final_num = 0 
+        for i in range(sos):
+            square = (i + 1) * (i + 1)
+            final_num = square + final_num
+        print(final_num)
